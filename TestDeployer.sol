bca5da9494e6c86b4b26970fac6a7f0a31b441b7
Ahmad Rafi Wiratmoko <45526567+thepirateboy@users.noreply.github.com>
2022-07-12 11:03:02 +1000

    Create TestDeployer.sol

---
+++ TestDeployer.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract GameItems is ERC1155, Ownable{
+
+
+    mapping (uint256 => string) private _uris;
+
+
+    constructor() ERC1155("https://bafybeif6gsqjhcnaypxtjoh43tcjca2kxrqxb6paxi4gjoevuasjmka5py.ipfs.nftstorage.link/{i}.json"){
+        for (uint i = 0; i<20; i++){
+            uint256 MyNFT = i+1;
+            _mint(msg.sender, MyNFT, 1, "");
+        }
+    }
+
+
+    function uri(uint256 _tokenId) override public view returns (string memory){
+        return string(
+            abi.encodePacked(
+                "https://bafybeif6gsqjhcnaypxtjoh43tcjca2kxrqxb6paxi4gjoevuasjmka5py.ipfs.nftstorage.link/",
+                Strings.toString(_tokenId),
+                ".json"
+            )
+        );
+    }
+}
