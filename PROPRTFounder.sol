dfb8e36693a7d6254ce54b7f526c7091bb9afc8f
Ahmad Rafi Wiratmoko <45526567+thepirateboy@users.noreply.github.com>
2022-07-25 18:09:20 +1000

    new

--- PROPRTFounder.sol
+++ PROPRTFounder.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract PROPRTFounder is ERC1155, Ownable{
+contract PROPRTFounder is ERC2981, ERC1155, Ownable{
 
     mapping (uint256 => string) private _uris;
     string public name = "PROPRT Founder";
@@ -28,4 +29,17 @@ contract PROPRTFounder is ERC1155, Ownable{
             )
         );
     }
+
+    
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC2981, ERC1155) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function royaltyInfo(
+        uint256 _tokenId,
+        uint256 _value
+    ) external view returns (
+        address _receiver,
+        uint256 _royaltyAmount
+    );
 }
