112a5bce64f9edec2b8ded96b88f28df5c35de4c
Rafi <45526567+thepirateboy@users.noreply.github.com>
2022-08-04 11:52:06 +1000

    Update for pre-launch

--- PROPRTFounder.sol
+++ PROPRTFounder.sol
@@ -15,8 +15,10 @@ contract PROPRTFounder is ERC1155, ERC2981, Ownable{
     string public name = "PROPRT Founder";
     string public symbol = "PROPRTFounder";
 
+    // call constructor and loop minting from 1 into 444 of metadata files.
+    // Set the roaylty into 5%
     constructor() ERC1155("https://proprt.mypinata.cloud/ipfs/QmUF9Kvr9wwoCQ21zbMDcjDbRyYKiTEPkZs5UUgyHfq1XA/{i}.json"){
-        for (uint i = 0; i<20; i++){
+        for (uint i = 0; i<444; i++){
             uint256 PROPRTFounder = i+1;
             _mint(msg.sender, PROPRTFounder, 1, "");
             _setDefaultRoyalty(_msgSender(), 500);
